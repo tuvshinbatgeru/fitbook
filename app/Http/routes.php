1d37f31c15206f134d2d8650f050ebac552d872c
AkilLis <g.tuvshinbat@yahoo.com>
2016-08-19 17:21:23 +0900

    route added

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,103 +14,94 @@ use Illuminate\Http\Request;
 |
 */
 
-//test
-Route::get('/resizeImage', 'FileManagerController@resizeImage');
-Route::post('/resizeImagePost',['as'=>'resizeImagePost','uses'=>'FileManagerController@resizeImagePost']);
-
-Route::get('/', function () 
-{
+Route::get('/', function () {
     return view('index');
 });
 
-Route::get('/dashboard', function() 
-{
-    return view('dashboard');
-});
-
-/* api cals */
-/* type - club */
-
 Route::get('/api/search', 'SearchController@search');
-
-Route::get('/api/club/club-info/{clubid}', 'ClubController@info');
-Route::get('/api/club/follow/{club}', 'UserController@toggleFollow');
-Route::get('/api/club/request/{club}', 'UserController@toggleRequest');
-Route::get('/api/club/teacher/{club}', 'ClubController@getTeachers');
-
-Route::get('/api/club/edit/{club}/index', 'ClubEditController@index');
-Route::get('/api/club/edit/{club}/member', 'ClubEditController@member');
-Route::get('/api/club/edit/{club}/request', 'ClubEditController@request');
-Route::put('/api/club/edit/{club}/request/{user}', 'ClubEditController@requestResponse');
-
-
-/*Route::get('/test/{userId}', function($userId)
-{
-	return $userId;
-})->middleware('throttle:3');*/
-
-
-
-Route::get('/create-club', function(Request $request) 
-{
-    return view('create-club');
+Route::get('/search', function(Request $request) {
+	    return view('search');
 });
-
-Route::get('/search', function(Request $request) 
-{
-    return view('search');
-});
-
-Route::get('/{club}', 'ClubController@index');
-Route::get('/{club}/edit', 'ClubEditController@edit');
 
 Route::post('auth/login', 'Auth\AuthController@login');
-Route::get('auth/logout', 'Auth\AuthController@logout');
-
-Route::get('auth/login', function(){
-	return view('auth.login');
+Route::get('auth/login', function() {
+		return view('auth.login');
 });
 
-Route::get('auth/verify-account', function() 
-{
-    return view('auth.verify-account');
+Route::group(['middleware' => ['web']], function () {
+
+	Route::get('/dashboard', function() {
+	    return view('dashboard');
+	});
+
+	/* api cals */
+	/* type - club */
+	Route::post('/api/test','FileManagerController@test');
+	Route::post('/upload', 'FileManagerController@upload');
+
+	//Club info APIs
+	Route::group(['prefix' => '/api/club/{club}/'], function () {
+
+		Route::get('club-info', 'ClubController@info');
+		Route::get('follow', 'UserController@toggleFollow');
+		Route::get('request', 'UserController@toggleRequest');
+		Route::get('teacher', 'ClubController@getTeachers');
+
+		Route::resource('training', 'TrainingController');
+		Route::resource('loyalty', 'LoyaltyController');
+		Route::resource('widgets', 'TemplateController');
+	});
+
+	//Club edit APIs
+	Route::group(['prefix' => '/api/club/edit/{club}/'], function () {
+
+		Route::get('index', 'ClubEditController@index');
+		Route::get('member', 'ClubEditController@member');
+		Route::get('request', 'ClubEditController@request');
+		Route::put('request/{user}', 'ClubEditController@requestResponse');	
+
+	});
+
+	/* type - user */
+
+	Route::get('/api/user/files', 'FileManagerController@files');
+	Route::get('/create-club', function(Request $request) {
+	    return view('create-club');
+	});
+
+	Route::get('/{club}', 'ClubController@index');
+	Route::get('/{club}/edit', 'ClubEditController@edit');
+	Route::get('auth/logout', 'Auth\AuthController@logout');
+	Route::get('auth/verify-account', function() {
+	    return view('auth.verify-account');
+	});
+
+	Route::post('auth/activate', function(Request $request) {
+		if(User::checkUserAvailable($request->username))
+			return redirect('/');
+
+		$user = new User();
+	    $user->fill($request->all());
+		$user->is_active = 'Y';
+		$user->save();
+		Auth::login($user);
+	    return redirect('/');
+	});
+
+	Route::get('/api/check-user/{username}', function($username) {
+		if(User::checkUserAvailable($username))
+			return Response::json(['result' => 'no']);
+	    return Response::json(['result' => 'ok']);
+	});
+
+	//Social Login
+	Route::get('/login/{provider?}',[
+	    'uses' => 'Auth\AuthController@getSocialAuth',
+	    'as'   => 'auth.getSocialAuth'
+	]);
+
+	Route::get('/login/callback/{provider?}',[
+	    'uses' => 'Auth\AuthController@getSocialAuthCallback',
+	    'as'   => 'auth.getSocialAuthCallback'
+	]);
 });
-
-/*Route::get('auth/activate', function(){
-	return view('/dashboard');
-});*/
-
-Route::post('auth/activate', function(Request $request) 
-{
-	/*dd($user);*/
-	/*$user = $request->user;
-	$user->save();
-	Auth::login($user);*/
-	if(User::checkUserAvailable($request->username))
-		return redirect('/');
-
-	$user = new User();
-    $user->fill($request->all());
-	$user->is_active = 'Y';
-	$user->save();
-	Auth::login($user);
-    return redirect('/');
-});
-
-Route::get('/api/check-user/{username}', function($username) 
-{
-	if(User::checkUserAvailable($username))
-		return Response::json(['result' => 'no']);
-    return Response::json(['result' => 'ok']);
-});
-
-//Social Login
-Route::get('/login/{provider?}',[
-    'uses' => 'Auth\AuthController@getSocialAuth',
-    'as'   => 'auth.getSocialAuth'
-]);
-
-Route::get('/login/callback/{provider?}',[
-    'uses' => 'Auth\AuthController@getSocialAuthCallback',
-    'as'   => 'auth.getSocialAuthCallback'
-]);
