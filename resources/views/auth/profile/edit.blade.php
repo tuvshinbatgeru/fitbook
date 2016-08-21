8ad1da4908ad9598e5b43b6c3a6fdc0f6c52fb03
AkilLis <g.tuvshinbat@yahoo.com>
2016-08-21 22:30:19 +0900

    user edit page added

---
+++ resources/views/auth/profile/edit.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.master-layout', [
+	'currentView' => 'profile-edit-view',
+	'user' => Auth::user()
+	])
+
+@section('content')
+
+
+<custom-modal 
+	:id = "{{$user->id}}" 
+	type = "User"
+	title ="Photo chooser" 
+	usage = "_photo-chooser" 
+	:show.sync = "showFileManager"
+	save-callback = "chooseAvatar"
+	context = "fileManager">
+</custom-modal>
+
+
+<form method="POST">
+
+	<div style="height:100px; width:1000px; background-color:#aecaec;">
+		what about now
+	</div>	
+	<input type="text" name="firstname" v-model="user.first_name">	
+	<input type="text" name="lastname" v-model="user.last_name">	
+
+	<p>Flex Gym, Mongolia</p>
+
+
+	<button @click="uploadAvatar($event)" style="position:relative; height: 168px; width: 168px; ">
+	    <img v-bind:src="user.avatar_url" style="width: 168px;
+	    	height: 168px;
+	    	position: absolute;
+	    	top: 0;
+	    	z-index: 1;
+	    	left: 0;
+	    	border-radius: 168px; border: 4px solid #5fcf80;" />
+		
+		<i class="fa fa-camera" style="position: absolute; 
+		color:white; 
+		font-size: 24px;
+		left: 42%; 
+		top : 80%; 
+		z-index:2;">
+	    </i>
+	</button>
+
+	<button class="button success" type="submit">Update</button>
+</form>
+
+@stop
