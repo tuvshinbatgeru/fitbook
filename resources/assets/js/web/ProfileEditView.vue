e7d1ea70ab25c9cdbc786ca7c506be64cfa4c080
AkilLis <g.tuvshinbat@yahoo.com>
2016-08-21 22:29:42 +0900

    Profile list, edit

---
+++ resources/assets/js/web/ProfileEditView.vue
@@ -0,0 +1,44 @@
+<script>
+	export default {
+		props: { 
+			title : { default : 'test' },
+			user : {}
+		},
+
+		data() {
+			return {
+				showFileManager : false
+			}
+		},
+		
+		created : function () {
+
+		},
+
+		events : {
+			'chooseAvatar' : function($response) {
+				this.chooseAvatar($response);
+			}
+		},
+
+		methods : {
+			uploadAvatar : function (event) {
+				event.preventDefault()
+				this.showFileManager = true; 
+			},
+
+			chooseAvatar : function($response) {
+				var photo = $response.data;
+				if(photo.length > 0) {
+					this.$http.post(this.$env.get('APP_URI') + 'api/user/avatar/' + photo[0].id).then(res => {
+						this.user.avatar_url = res.data.avatar;	
+					}).catch(err => {
+
+					});
+				}
+				
+				this.showFileManager = false;
+			}
+		}	
+	}
+</script>
---
+++ resources/assets/js/web/ProfileView.vue
@@ -0,0 +1,48 @@
+<script>
+	var data = {
+	    labels: ["January", "February", "March", "April", "May", "June", "July"],
+	    datasets: [
+	        {
+	            label: "My First dataset",
+	            backgroundColor: [
+	                'rgba(255, 99, 132, 0.2)',
+	                'rgba(54, 162, 235, 0.2)',
+	                'rgba(255, 206, 86, 0.2)',
+	                'rgba(75, 192, 192, 0.2)',
+	                'rgba(153, 102, 255, 0.2)',
+	                'rgba(255, 159, 64, 0.2)'
+	            ],
+	            borderColor: [
+	                'rgba(255,99,132,1)',
+	                'rgba(54, 162, 235, 1)',
+	                'rgba(255, 206, 86, 1)',
+	                'rgba(75, 192, 192, 1)',
+	                'rgba(153, 102, 255, 1)',
+	                'rgba(255, 159, 64, 1)'
+	            ],
+	            borderWidth: 1,
+	            data: [65, 59, 80, 81, 56, 55, 40],
+	        }
+	    ]
+	};
+
+	export default {
+		props: { 
+			title : { default : 'test' },
+		},
+
+		ready : function () {
+			var myBarChart = new Chart(document.querySelector('#chart').getContext('2d'), {
+			    type: 'bar',
+			    data: data,
+			     options: {
+			        scales: {
+			            yAxes: [{
+			                stacked: true
+			            }]
+			        }
+			    }
+			});
+		}
+	}
+</script>
