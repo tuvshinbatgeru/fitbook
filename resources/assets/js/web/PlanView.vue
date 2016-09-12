c869ac8e999f6956156ee70767e83db0eb6ecc0c
AkilLis <g.tuvshinbat@yahoo.com>
2016-09-12 21:58:17 +0900

    laravel scout

---
+++ resources/assets/js/web/PlanView.vue
@@ -0,0 +1,31 @@
+<script>
+	export default {
+
+		props: { 
+			
+		},
+
+		data () {
+			return {
+				showSubscription : false,
+			}
+		},
+
+		methods : {
+			setSubscription : function (type) {
+				this.showSubscription = type;
+			}	
+		},
+
+		created : function () {
+
+		},
+
+		ready : function () {
+			
+		},
+
+		components : {
+		},
+	}
+</script>
