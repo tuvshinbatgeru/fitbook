8e0ded07fee69b4d7b0999f27069796d136c641b
AkilLis <g.tuvshinbat@yahoo.com>
2016-09-13 20:56:50 +0900

    autolock

--- resources/assets/js/web/PlanView.vue
+++ resources/assets/js/web/PlanView.vue
@@ -1,4 +1,6 @@
 <script>
+	import AddSubscriptions from '../context/AddSubscriptions.vue'; 
+
 	export default {
 
 		props: { 
@@ -11,10 +13,24 @@
 			}
 		},
 
+		events : {
+		    'saveSubscription' : function($response) {
+		        this.saveSubscription($response);
+		    },
+		},
+
 		methods : {
 			setSubscription : function (type) {
 				this.showSubscription = type;
-			}	
+			}, 
+
+			saveSubscription : function($response) {
+				this.$http.post(this.$env.get('APP_URI') + 'subscriptions?data=' + $response.data.param).then(res => {
+					this.showSubscription = false;  	
+				}).catch(err => {
+				  	
+				});
+			},
 		},
 
 		created : function () {
@@ -26,6 +42,7 @@
 		},
 
 		components : {
+			AddSubscriptions
 		},
 	}
 </script>
