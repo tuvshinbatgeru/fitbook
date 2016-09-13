1ec911e480f7a49ae2555965fc2d3af08c9449f3
AkilLis <g.tuvshinbat@yahoo.com>
2016-09-13 20:56:03 +0900

    base Custom Callout added

---
+++ resources/assets/js/actors/application/components/CustomCallout.vue
@@ -0,0 +1,40 @@
+<template>
+	<div v-show="!loading" v-if="!blank" class="callout primary">
+		<slot>
+		</slot>
+	</div>
+
+	<div v-show="loading" v-if="!blank" class="callout secondary">
+		<div class="preloader-wrapper small active">
+	      <div class="spinner-layer spinner-green-only">
+	        <div class="circle-clipper left">
+	          <div class="circle"></div>
+	        </div><div class="gap-patch">
+	          <div class="circle"></div>
+	        </div><div class="circle-clipper right">
+	          <div class="circle"></div>
+	        </div>
+	      </div>
+	    </div>
+	</div>
+</template>
+
+<script>
+
+	export default {
+		props : {
+			loading : {
+				type: Boolean,
+				default : false
+			},
+
+			blank: {
+				type: Boolean,
+				default : true,
+			}
+		},
+	}
+</script>
+
+<style>
+</style>
