e75453a16901f8750686bbc3545ab5b40e6c5169
AkilLis <g.tuvshinbat@yahoo.com>
2016-09-13 20:55:50 +0900

    QueryFilter init

---
+++ app/Filters/QueryFilter.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Filters;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\Request;
+
+abstract class QueryFilter {
+
+	protected $request;
+
+	protected $builder;
+
+	public function __construct(Request $request)
+	{
+		$this->request = $request;
+	}
+
+	public function apply($builder)
+	{
+		$this->builder = $builder;
+
+		foreach ($this->filters() as $name => $value) {
+			if(method_exists($this, $name)) {
+				call_user_func_array([$this, $name], array_filter([$value]));
+			}
+		}
+
+		return $this->builder;
+	}
+
+	public function filters()
+	{
+		return $this->request->all();
+	}
+}
