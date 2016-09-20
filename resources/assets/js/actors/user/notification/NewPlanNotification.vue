f2b6b7b0e8e95cb3fb39c9b9b74c415eb9e60150
AkilLis <g.tuvshinbat@yahoo.com>
2016-09-20 19:02:16 +0900

    NewPlan notification template added

---
+++ resources/assets/js/actors/user/notification/NewPlanNotification.vue
@@ -0,0 +1,25 @@
+<template>
+	<h4>{{notification.data.name}}</h4>
+	<label>{{notification.data.club_short_name}}</label>
+	<p>{{notification.data.description}}</p>
+	<p>{{notification.data.release_date.date | moment "from" }}</p>
+
+	<a>Mark as read</a>
+
+</template>
+
+<script>
+	export default {
+		props: {
+			notification : {
+				required : true
+			},
+		},
+		
+		methods : {
+
+		},
+	}
+</script>
+<style>
+</style>
