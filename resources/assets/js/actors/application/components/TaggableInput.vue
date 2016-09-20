bcc67972eb04a140db9404ea545d96375f468049
AkilLis <g.tuvshinbat@yahoo.com>
2016-09-20 19:02:26 +0900

    user main header taggable,,

---
+++ resources/assets/js/actors/application/components/TaggableInput.vue
@@ -0,0 +1,46 @@
+<template>
+	<div class="multiselect__tags">
+		<span class="multiselect__tag">
+			<input value="Test" />
+		</span>
+		<span class="multiselect__tag">
+			<input value=" " />
+		</span>
+		<span>
+			<input value="Value" />
+		</span>
+	</div>
+</template>
+
+<script>
+	export default {
+		props : {
+			debounce : {
+				default : 300,
+			},
+
+			tags : {
+
+			},
+
+			input : {},
+		},
+	}
+</script>
+
+<style>
+
+.multiselect__tags {
+  min-height: 40px;
+  display: block;
+  padding: 8px 40px 0 8px;
+  border-radius: 5px;
+  border: 1px solid #E8E8E8;
+  background: #fff;
+}
+
+.multiselect__tag ~ .multiselect__input {
+  width: auto;
+}
+
+</style>
---
+++ resources/assets/js/actors/user/components/MainHeader.vue
@@ -0,0 +1,23 @@
+<script>
+
+	//global context
+	import Notifications from '../../../context/notifications.vue';
+
+	export default {
+		props: { 
+			user : { 
+				required : true
+			},
+		},
+
+		data () {
+			return {
+				showNotifications : false,
+			}
+		},
+		
+		components : {
+            Notifications
+        }
+	}
+</script>
