27ba5ff035852c3892d8323ade01ccee03904a80
AkilLis <g.tuvshinbat@yahoo.com>
2016-09-21 21:02:46 +0900

    photo mention template

---
+++ resources/assets/js/actors/user/mention/PhotoMention.vue
@@ -0,0 +1,23 @@
+<template>
+	<h4>{{mention.mentioned.ext}}</h4>
+	<img style="height:50px; width:50px;" :src="mention.mentioned.url"/>
+
+	<a>Mark as read</a>
+
+</template>
+
+<script>
+	export default {
+		props: {
+			mention : {
+				required : true
+			},
+		},
+		
+		methods : {
+
+		},
+	}
+</script>
+<style>
+</style>
