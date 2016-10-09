20e239d94348ea0a372794eb3dc7655e22c7f0c3
AkilLis <g.tuvshinbat@yahoo.com>
2016-10-09 16:14:25 +0800

    user subscription pinned photo, club dtl photo

--- resources/assets/js/actors/user/components/LogoutMenus.vue
+++ resources/assets/js/actors/user/components/LogoutMenus.vue
@@ -1,21 +1,26 @@
 <template>
-	Your clubs 
-    <br>
-    <ul>
-	    <li v-for="menu in menus">
-	        <a :href="'/' + menu.club_id + '/edit'">
-	          {{menu.short_name}}
-	        </a>
-	    </li>
-    </ul>
-    Subscriptions 
-    <br>
-    <ul>
-    	<li v-for="subscription in subscriptions">
-    		<a>{{subscription.name}}</a>		
-    	</li>
-    </ul>
-    <a href="/auth/logout">Log out</a>  
+	<div class="logout__menu">
+		Membership clubs
+	    <br>
+	    <ul>
+		    <li v-for="menu in menus">
+		    	<img src="/images/1470464066.png" style="height:32px; width:32px;" />
+		        <a :href="'/' + menu.club_id + '/edit'">
+		          {{menu.short_name}} as Manager
+		        </a>
+		    </li>
+	    </ul>
+	    Subscriptions 
+	    <br>
+	    <ul>
+	    	<li v-for="subscription in subscriptions">
+	    		<img :src="subscription.pinned_photos[0].url" style="height:32px; width:32px;" />
+	    		<a>{{subscription.name}}</a>
+	    		<span style="padding:5px; border-radius:4px; background-color:red; color:white;">{{subscription.pivot.end_date | dayDiffFromNow}}</span>days left
+	    	</li>
+	    </ul>
+	    <a href="/auth/logout">Log out</a>  
+	</div>
 </template>
 <script>
 	export default {
@@ -29,6 +34,12 @@
 			return {
 				menus : [],
 				subscriptions : [],
+			}
+		},
+
+		filters : {
+			dayDiffFromNow : function (from) {
+				return Vue.moment(from, 'YYYY-MM-DD').diff(Vue.moment(), 'days');
 			}
 		},
 
@@ -49,3 +60,8 @@
 		}
 	}
 </script>
+<style type="text/css">
+	.logout__menu {
+		font-size:12px;
+	}
+</style>
