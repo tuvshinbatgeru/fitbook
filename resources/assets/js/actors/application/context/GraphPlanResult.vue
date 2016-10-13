24dc96f40b9c8328c371d86343da742c2c4ed0c1
AkilLis <g.tuvshinbat@yahoo.com>
2016-10-13 12:10:22 +0800

    Graph plan, user result vue added

---
+++ resources/assets/js/actors/application/context/GraphPlanResult.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="Typeahead__content">
+		<h4>Plan</h4>
+	    <img :src="item.avatar_url" style="height:32px; width:32px; border-radius: 32px;" />
+	    <span class="name" v-text="item.username"></span>
+	    <span class="screen-name" v-text="item.first_name"></span>
+	</div>
+</template>
+<script>
+export default {
+	props : {
+		item : {
+			required : true,
+		}
+	},
+}
+</script>
+<style>
+
+.Typeahead__content span {
+    display: block;
+    color: #2c3e50;
+}
+
+</style>
