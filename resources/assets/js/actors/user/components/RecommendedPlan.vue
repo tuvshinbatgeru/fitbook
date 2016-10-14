7433a43934121695729ac367aafec1567db2d6e9
AkilLis <g.tuvshinbat@yahoo.com>
2016-10-14 17:26:24 +0800

    RecommendedPlan

---
+++ resources/assets/js/actors/user/components/RecommendedPlan.vue
@@ -0,0 +1,30 @@
+<template>
+    <h3>Recommended plans</h3>
+</template>
+
+<script>
+    export default {
+        
+        props: { 
+            id : {},
+        },
+
+        data () {
+            return {
+
+            }
+        },
+
+        created : function () {
+            
+        },
+
+        ready : function () {
+
+        },
+
+        methods : {
+            
+        },
+    }
+</script>
