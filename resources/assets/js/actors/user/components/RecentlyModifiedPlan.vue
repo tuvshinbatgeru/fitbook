25899741a4a7823b5cf6e07780b087d14ee83c2a
AkilLis <g.tuvshinbat@yahoo.com>
2016-10-14 17:26:18 +0800

    RecentlyModifiedPlan

---
+++ resources/assets/js/actors/user/components/RecentlyModifiedPlan.vue
@@ -0,0 +1,30 @@
+<template>
+    <h3>Recently Added plans</h3>
+</template>
+
+<script>
+    export default {
+        
+        props: { 
+            id : {},
+        },
+
+        data () {
+            return {
+
+            }
+        },
+
+        created : function () {
+            
+        },
+
+        ready : function () {
+
+        },
+
+        methods : {
+            
+        },
+    }
+</script>
