5b001346be0fe80c9160164a08bc0598f7621929
AkilLis <g.tuvshinbat@yahoo.com>
2016-10-14 17:26:32 +0800

    UserHome added

---
+++ resources/assets/js/actors/user/components/UserHome.vue
@@ -0,0 +1,40 @@
+<template>
+    <recently-modified-plan :id="id"></recently-modified-plan>
+    <recommended-plan :id="id"></recommended-plan>
+    <nearby-plan :id="id"></nearby-plan>
+</template>
+
+<script>
+    import RecentlyModifiedPlan from './RecentlyModifiedPlan.vue'
+    import RecommendedPlan from './RecommendedPlan.vue'
+    import NearbyPlan from './NearbyPlan.vue'
+
+    export default {
+        
+        props: { 
+            id : {},
+        },
+
+        data () {
+            return {
+
+            }
+        },
+
+        created : function () {
+            
+        },
+
+        ready : function () {
+
+        },
+
+        methods : {
+            
+        },
+
+        components : {
+            RecentlyModifiedPlan, RecommendedPlan, NearbyPlan
+        }
+    }
+</script>
