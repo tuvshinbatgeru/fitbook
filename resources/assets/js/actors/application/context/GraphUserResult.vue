12a73a91b933b53d18aac07c163dc77bc62dee77
AkilLis <g.tuvshinbat@yahoo.com>
2016-10-18 21:49:40 +0800

    UserGrap REsult

--- resources/assets/js/actors/application/context/GraphUserResult.vue
+++ resources/assets/js/actors/application/context/GraphUserResult.vue
@@ -1,9 +1,16 @@
 <template>
-	<div class="Typeahead__content">
-		<h4>User</h4>
-	    <img :src="item.avatar_url" style="height:32px; width:32px; border-radius: 32px;" />
-	    <span class="name" v-text="item.username"></span>
-	    <span class="screen-name" v-text="item.first_name"></span>
+	<div class="Typeahead__content row">
+		<div class="small-3 columns">
+			<img :src="avatar" 
+				 style="height:32px; width:32px; border-radius: 32px;" 
+				 class="text-right" />	
+		</div>
+		<div class="small-9 columns">
+			<div class="row text-left">
+				<span class="name" v-text="item.username"></span>
+		    	<span class="screen-name" v-text="item.first_name"></span>	
+	    	</div>
+		</div>
 	</div>
 </template>
 <script>
@@ -11,8 +18,14 @@ export default {
 	props : {
 		item : {
 			required : true,
-		}
+		},
 	},
+
+	computed : {
+		avatar : function () {
+			return this.item.avatar_small.length == 0 ? this.$env.get('APP_URI') + 'images/test_person.jpg' : this.item.avatar_small[0].url;
+		}
+	}
 }
 </script>
 <style>
