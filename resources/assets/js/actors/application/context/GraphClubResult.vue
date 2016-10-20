a81bd412ed079e45482174fdeb886ffdcb95db14
AkilLis <g.tuvshinbat@yahoo.com>
2016-10-20 20:20:51 +0800

    Graph CLub Result added

---
+++ resources/assets/js/actors/application/context/GraphClubResult.vue
@@ -0,0 +1,45 @@
+<template>
+	<div class="Typeahead__content row">
+		<div class="small-3 columns">
+			<img :src="url" 
+				 style="height:32px; width:32px; border-radius: 32px;" 
+				 class="text-right" />	
+		</div>
+		<div class="small-9 columns">
+			<div class="row text-center">
+				<span class="Typeahead__badge">
+					CLUB
+				</span>
+			</div>
+			<div class="row text-left Typeahead__content">
+				<span class="name" v-text="item.searchable.short_name"></span>
+		    	<span class="screen-name" v-text="item.searchable.full_name"></span>	
+	    	</div>
+		</div>
+	</div>
+</template>
+<script>
+export default {
+	props : {
+		item : {
+			required : true,
+		},
+	},
+
+	computed : {
+		url : function () {
+			return this.$env.get('APP_URI') + 'images/1470464066.png';
+		}
+	}
+}
+</script>
+<style scoped>
+
+.Typeahead__badge {
+	padding: 5px;
+	border-radius : 5px;
+	background-color: #1bb1b2;
+	color: white;
+	font-weight: bold;
+}
+</style>
