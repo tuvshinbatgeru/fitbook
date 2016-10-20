6328515999276813ef1d8f3fff08619fe6563882
AkilLis <g.tuvshinbat@yahoo.com>
2016-10-20 12:03:51 +0800

    loadable, color, feature added

--- resources/assets/js/actors/application/components/CustomCallout.vue
+++ resources/assets/js/actors/application/components/CustomCallout.vue
@@ -1,12 +1,20 @@
 <template>
-	<div v-show="!loading" v-if="!blank" class="callout primary">
-		<slot>
+	<div v-show="!loading"
+	     class="callout" 
+	     :class="type | lowercase">
+		<button v-show="closeable" class="callout__close" aria-label="Dismiss alert" type="button" data-close>
+	    	<span aria-hidden="true">&times;</span>
+	  	</button>
+		<slot name="content">
 		</slot>
 	</div>
 
-	<div v-show="loading" v-if="!blank" class="callout secondary">
-		<div class="preloader-wrapper small active">
-	      <div class="spinner-layer spinner-green-only">
+	<div v-show="loading" 
+		 :style="'height:' + height + 'px;'"
+		 :class="type | lowercase"
+		 class="callout text-center">
+		<div :class="spinnerType" class="preloader-wrapper active">
+	      <div class="spinner-layer" :style="'border-color: ' + spinnerColor">
 	        <div class="circle-clipper left">
 	          <div class="circle"></div>
 	        </div><div class="gap-patch">
@@ -27,14 +35,76 @@
 				type: Boolean,
 				default : false
 			},
+			
+			type : {
+				type: String,
+				default : 'main'
+			},
+			
+			borderRadius : {
+				type: Number,
+				default : 5,
+			},
 
-			blank: {
+			spinnerType : {
+				type: String,
+				default : 'small',
+			},
+
+			spinnerColor : {
+				type: String, 
+				default : '#3f4652',
+			},
+
+			height : {
+				type: Number,
+				default : '300',
+			},
+
+			closeable : {
 				type: Boolean,
-				default : true,
+				default : false,
 			}
 		},
 	}
 </script>
 
-<style>
+<style lang="scss">
+
+.callout { 
+	position: relative;
+}
+
+.callout__close {
+	position: absolute;
+	top : 0;
+	right: 0;
+}
+
+.callout {
+	.preloader-wrapper {
+		top: 45%;
+	}
+
+	.main {
+		background-color: #fff;
+	}
+
+	.small {
+		width: 20px;
+	    height: 20px;
+	}
+	
+	.medium {
+		width: 32px;
+		height: 32px;
+	}
+
+	.large {
+		width: 64px;
+		height: 64px;
+	}
+}
+
+
 </style>
