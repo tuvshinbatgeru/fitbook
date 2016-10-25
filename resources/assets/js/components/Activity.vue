49276df746ffed73ecf10016442db3680552c43f
AkilLis <g.tuvshinbat@yahoo.com>
2016-10-25 13:48:49 +0800

    Custom calendar implemented

--- resources/assets/js/components/Activity.vue
+++ resources/assets/js/components/Activity.vue
@@ -1,11 +1,21 @@
 <template>
+    <div class="Activity__Calendar">
+        <custom-calendar :user-id="id" :year="2016">
+            
+        </custom-calendar>
+    </div>
+
     <div class="row" v-for="activity in activities">
         <h3>{{activity.short_name}}</h3>
         <label>{{activity.pivot.duration}} min</label>
+
+       
     </div>
 </template>
 
 <script>
+    import CustomCalendar from '../actors/application/svg/CustomCalendar.vue';
+
     export default {
         
         props: { 
@@ -14,12 +24,12 @@
 
         data () {
             return {
-                activities : [],
+
             }
         },
 
         created : function () {
-            this.getActivity();
+
         },
 
         ready : function () {
@@ -27,12 +37,18 @@
         },
 
         methods : {
-            getActivity : function () {
-                this.$http.get(this.$env.get('APP_URI') + 'api/user/' + this.id + '/activity').then(res => {
-                    this.activities = res.data.result;
-                }).catch(err => {
-                });
-            }
+            
         },
+
+        components : {
+            CustomCalendar
+        }
     }
 </script>
+<style lang="scss">
+    .Activity__Calendar {
+        width: 100%;
+        overflow-y: hidden;
+        overflow-x: auto;
+    }
+</style>
