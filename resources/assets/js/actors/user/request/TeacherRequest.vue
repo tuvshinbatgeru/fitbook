306eb3f2f7ccdd8929b1fc632f91c2967ccf6648
AkilLis <g.tuvshinbat@yahoo.com>
2016-10-27 14:52:50 +0800

    TeacherRequest added

---
+++ resources/assets/js/actors/user/request/TeacherRequest.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="request">
+		<h4>Request</h4>
+		<a>Mark as read</a>
+	</div>
+</template>
+
+<script>
+	export default {
+		props: {
+			request : {
+				required : true
+			},
+		},
+	}
+</script>
