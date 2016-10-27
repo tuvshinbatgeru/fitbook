bb24d77a0cbe83c0aae3476904d77014dc234149
AkilLis <g.tuvshinbat@yahoo.com>
2016-10-27 14:53:50 +0800

    notifications added

--- resources/assets/js/context/notifications.vue
+++ resources/assets/js/context/notifications.vue
@@ -2,11 +2,14 @@
 	<ul class="tabs">
 	    <li class="tab s3">
 	    	<a @click="getNotifications()" class="active" href="#notifications">
-	    		All
+	    		{{$t('all')}}
 	    	</a>
 	    </li>
 	    <li class="tab s3">
-	    	<a @click="getMentions()" href="#mentions">Mentions</a>
+	    	<a @click="getMentions()" href="#mentions">{{$t('mention')}}</a>
+	    </li>
+	    <li class="tab s3">
+	    	<a @click="getRequests()" href="#requests">{{$t('request')}}</a>
 	    </li>
 	</ul>
 
@@ -27,7 +30,17 @@
 						v-for="mention in mentions">
 			</components>
 		</ul>
-		<h3 v-show="mentions.length == 0">There is no mention</h3>
+		<h3 v-show="mentions.length == 0">There is no mentions</h3>
+	</div>
+
+	<div id="requests">
+		<ul v-show="requests.length != 0">
+			<components :request="request"
+						:is="requestComponentFilter(request)"
+						v-for="request in requests">
+			</components>
+		</ul>
+		<h3 v-show="requests.length == 0">There is no requests</h3>
 	</div>
 </template>
 
@@ -38,6 +51,8 @@
 
 	import PhotoMention from '../actors/user/mention/PhotoMention.vue';
 	import CommentMention from '../actors/user/mention/CommentMention.vue';
+
+	import TeacherRequest from '../actors/user/request/TeacherRequest.vue'
 	
 	export default {
 		props: {
@@ -48,6 +63,7 @@
 			return {
 				notifications : [],
 				mentions : [],
+				requests : [],
 			}			
 		},
 
@@ -66,36 +82,57 @@
 						return "photo-mention";
 					case "App\\Comment" :
 						return "comment-mention";
-					break;
+					default :
+						break
+				}
+			},
+
+			requestComponentFilter : function (request) {
+				switch(request.type) {
+					case 3 :  
+						return "teacher-request"
+					case 4 : 
+						return "manager-request"
+					case 5 :
+						return "reception-request"
 				}
 			},
 
 			notificationComponentFilter : function (notification) {
 				switch (notification.type) {
 					case "App\\Notifications\\NewPlan" : 
-						return "new-plan-notification";
+						return "new-plan-notification"
 					case "App\\Notifications\\TeacherRequestRecieved" :
-					 	return "teacher-request-recieved-notification";
-					break;
+					 	return "teacher-request-recieved-notification"
+					default : 
+						break
 				}
 			},
 
 			getNotifications : function () {
 				this.$http.get(this.$env.get('APP_URI') + 'api/user/' + this.userId + '/notifications').then(res => {
-				  	debugger;
 				  	if(res.data.code == 0) {
-				  		this.notifications = res.data.result;
+				  		this.notifications = res.data.result
 				  	}
 				}).catch(err => {
 				  
 				});
 			},
+			
+			getRequests : function () {
+				this.$http.get(this.$env.get('APP_URI') + 'api/user/' + this.userId + '/requests').then(res => {
+				  	if(res.data.code == 0) {
+				  		this.requests = res.data.result
+				  	}
+				}).catch(err => {
+
+				});
+			},
 
 			getMentions : function () {
 				this.$http.get(this.$env.get('APP_URI') + 'api/user/' + this.userId + '/mentions').then(res => {
-				  	debugger;
 				  	if(res.data.code == 0) {
-				  		this.mentions = res.data.result;
+				  		this.mentions = res.data.result
 				  	}
 				}).catch(err => {
 				  
@@ -105,7 +142,21 @@
 
 		components : {
 			NewPlanNotification, TeacherRequestRecievedNotification,
-			PhotoMention, CommentMention
+			PhotoMention, CommentMention, TeacherRequest
+		},
+
+		locales : {
+			en : {
+				mention : 'Mentions',
+				all : 'All',
+				request : 'Requests'
+			},
+
+			mn : {
+				mention : 'Дурьдсан',
+				all : 'Бүгд',
+				request : 'Хүсэлтүүд'
+			}
 		}
 	}
 </script>
