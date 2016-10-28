b8cb4f407426a799bef392fe05bcd22ec2caa1b2
Saruul\Saruultugs <saruultugs.azgun@gmail.com>
2016-10-28 21:41:27 +0800

    user cover hesgiin zagvar

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,24 +1,38 @@
 @extends('layouts.master-layout', ['currentView' => 'profile-view'])
 @section('content')
 
-<div class="row" style="margin-bottom: 10px; height: 280px;position: relative;background-image: url('{{asset('images/site/cove_photo.jpg')}}'); background-position: center 50%;background-size: 100% auto">
-  <div class="small-9 text-center small-centered columns">
-    <div class="hexagon" style="background-image: url('{{$user->avatar_url}}')">
-      <div class="hexTop"></div>
-      <div class="hexBottom"></div>
+<div class="row" style="margin-bottom: 10px;">
+  <div class="user-cover">
+    <img src="{{asset('images/site/cove_photo.jpg')}}" class="cover-photo"/>
+    <div class="user-profile-picture">
+        <div class="hexagon" style="background-image: url('{{$user->avatar_url}}')">
+          <div class="hexTop"></div>
+          <div class="hexBottom"></div>
+        </div>
+        <div class="user-profile-name">
+          <span class="name">
+          {{$user->first_name}}
+          </span>
+          <br/>
+          <span class="role">
+          Flex Gym - Manager
+          </span>
+        </div>
+    	</div>
+    <div class="cover-buttons">
+      @if($editable)
+        <a class="cover-button">
+          <i class="fa fa-edit"></i> Following
+        </a>
+        <a class="cover-button">
+          <i class="fa fa-edit"></i> Change cover
+        </a>
+        <a href="{{$user->username}}/edit" class="cover-button">
+          Edit Profile
+        </a>
+      @endif
     </div>
-		<!-- <img src="{{$user->avatar_url}}" style="width: 130px;
-    height: 130px;
-    border-radius: 180px; border: 4px solid #5fcf80;" /> -->
-    <h5 style="color: #fff">
-        {{$user->first_name}} {{$user->last_name}}
-    </h5> 
-	</div>
-    @if($editable)
-      <a href="{{$user->username}}/edit" class="button success" style="position: absolute;bottom: 20px; right:  20px;margin-bottom: 0px;">
-        Edit Profile
-      </a>
-    @endif
+  </div>
 </div>
 
 <div class="row">
