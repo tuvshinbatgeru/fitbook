e61ecf0080ca7b4aaafdd7bdc98395d827ff580f
AkilLis <g.tuvshinbat@yahoo.com>
2016-10-30 21:20:11 +0800

    Following feature

--- resources/assets/js/web/ProfileView.vue
+++ resources/assets/js/web/ProfileView.vue
@@ -1,77 +1,56 @@
 <script>
-	var data = {
-	    labels: ["January", "February", "March", "April", "May", "June", "July"],
-	    datasets: [
-	        {
-	            label: "My First dataset",
-	            backgroundColor: [
-	                'rgba(255, 99, 132, 0.2)',
-	                'rgba(54, 162, 235, 0.2)',
-	                'rgba(255, 206, 86, 0.2)',
-	                'rgba(75, 192, 192, 0.2)',
-	                'rgba(153, 102, 255, 0.2)',
-	                'rgba(255, 159, 64, 0.2)'
-	            ],
-	            borderColor: [
-	                'rgba(255,99,132,1)',
-	                'rgba(54, 162, 235, 1)',
-	                'rgba(255, 206, 86, 1)',
-	                'rgba(75, 192, 192, 1)',
-	                'rgba(153, 102, 255, 1)',
-	                'rgba(255, 159, 64, 1)'
-	            ],
-	            borderWidth: 1,
-	            data: [65, 59, 80, 81, 56, 55, 40],
-	        }
-	    ]
-	};
-
-	import Followed from '../components/Followed.vue';
+	import UserFollowing from '../actors/user/components/UserFollowing.vue';
 	import Timeline from '../components/Timeline.vue';
 	import Activity from '../actors/user/components/Activity.vue';
 	import UserHome from '../actors/user/components/UserHome.vue';
 
-
 	export default {
 		props: { 
 			title : { default : 'test' },
-			submenu : { default : 'activity'}
+			submenu : { default : 'user-home'},
+			user : {}
 		},
 
 		data () {
 			return {
 				showNotifications : false,
+				showFollowing : false,
+				following_count : 3,
 			}
+		},
+
+		created : function () {
+			this.getFollowingCount()
 		},
 		
 		ready : function () {
 			$('ul.tabs').tabs();
-
-			/*var myBarChart = new Chart(document.querySelector('#chart').getContext('2d'), {
-			    type: 'bar',
-			    data: data,
-			     options: {
-			        scales: {
-			            yAxes: [{
-			                stacked: true
-			            }]
-			        }
-			    }
-			});*/
 		},
 
 		methods : {
+			getFollowingCount : function () {
+				this.$http.get(this.$env.get('APP_URI') + 'api/user/' + this.user.id + '/following/count').then(res => {
+				  	this.following_count = res.data.result
+				}).catch(err => {
+
+				});
+			},
+
 			setSubMenu : function(menu) {
 				this.submenu = menu;
 			},
 
 			test : function () {
 				alert('test');
+			},
+
+			toggleFollow : function (follow) {
+				this.following_count += follow == "follow" ? -1 : +1
 			}
 		},
 
 		components : {
-            Followed, Timeline, Activity, UserHome
+            UserFollowing, Timeline, Activity, UserHome
         }
 	}
 </script>
