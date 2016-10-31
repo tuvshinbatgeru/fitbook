449efc98523aecac0ac2e47036079c9135deea86
AkilLis <g.tuvshinbat@yahoo.com>
2016-10-31 19:56:14 +0800

    user_photos table created

---
+++ database/migrations/2016_10_31_162655_create_user_photos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserPhotosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_photos', function (Blueprint $table) {
+            $table->integer('user_id')->unsigned()->index();
+            $table->integer('photo_id')->unsigned()->index();
+            $table->tinyInteger('type')->default(0); //1 - cover
+            $table->tinyInteger('top')->default(0);
+            $table->tinyInteger('left')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_photos');
+    }
+}
