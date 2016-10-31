5ce4f46560383f5d312673d701a8477944444426
AkilLis <g.tuvshinbat@yahoo.com>
2016-10-31 19:57:01 +0800

    cover, following feature adde user profile

--- resources/assets/js/web/ProfileView.vue
+++ resources/assets/js/web/ProfileView.vue
@@ -1,12 +1,14 @@
 <script>
-	import UserFollowing from '../actors/user/components/UserFollowing.vue';
-	import Timeline from '../components/Timeline.vue';
-	import Activity from '../actors/user/components/Activity.vue';
-	import UserHome from '../actors/user/components/UserHome.vue';
+	import Pageable from '../actors/application/mixins/Pageable'
+	import UserFollowing from '../actors/user/components/UserFollowing.vue'
+	import Timeline from '../components/Timeline.vue'
+	import Activity from '../actors/user/components/Activity.vue'
+	import UserHome from '../actors/user/components/UserHome.vue'
+	import FileManager from '../context/FileManager.vue';
 
 	export default {
+		mixins : [Pageable],
 		props: { 
-			title : { default : 'test' },
 			submenu : { default : 'user-home'},
 			user : {}
 		},
@@ -15,24 +17,93 @@
 			return {
 				showNotifications : false,
 				showFollowing : false,
-				following_count : 3,
+				following_count : 0,
+				editCover : false, 
+				showCoverPhoto : false,
+				coverPhoto : null,
+				duplicatePhoto : null,
 			}
 		},
 
 		created : function () {
 			this.getFollowingCount()
+			this.getCoverPhoto()
 		},
 		
 		ready : function () {
 			$('ul.tabs').tabs();
 		},
 
+		/*filters : {
+			coverFilter : function(coverPhoto) {
+				if(coverPhoto instanceof Object) {
+					return coverPhoto.url
+				}
+				return this.coverPhoto
+			}
+		},*/
+
+		events : {
+			'chooseCover' : function ($response) {
+				var photo = $response.data;
+
+				if(photo.length > 0) {
+					this.coverPhoto = photo[0]
+					this.editCover = true
+				}
+
+				this.showCoverPhoto = false
+			}
+		},
+
 		methods : {
+
+			changeCover : function () {
+				this.showCoverPhoto = true
+			},
+
+			cancelCoverChange : function () {
+				this.editCover = false
+				this.coverPhoto = this.duplicatePhoto
+			},
+
+			saveCoverChange : function () {
+				var viewPort = this.$refs.covercropper.getViewPort()
+
+				this.$http.post(this.$env.get('APP_URI') + 'api/user/cover?photoId=' + this.coverPhoto.id + 
+					'&top='+ viewPort.top +'&left=' + viewPort.left).then(res => {
+					this.coverPhoto = res.data.result
+					this.coverPhoto.pivot = {
+						'top' : viewPort.top,
+						'left' : viewPort.left,
+					}
+
+					this.duplicatePhoto = this.coverPhoto
+
+					this.editCover = false
+				}).catch(err => {
+					this.editCover = false
+				});
+			},
+
 			getFollowingCount : function () {
 				this.$http.get(this.$env.get('APP_URI') + 'api/user/' + this.user.id + '/following/count').then(res => {
 				  	this.following_count = res.data.result
 				}).catch(err => {
 
+				});
+			},
+
+			getCoverPhoto : function () {
+				this.$http.get(this.$env.get('APP_URI') + 'api/user/' + this.user.id + '/cover').then(res => {
+				  	if(res.data.code == 0) {
+				  		this.coverPhoto = res.data.result
+				  		this.duplicatePhoto = res.data.result
+				  		return
+				  	}
+
+				  	this.coverPhoto = this.$env.get('APP_URI') + 'images/site/default_cover.jpg'
+				}).catch(err => {
 				});
 			},
 
@@ -50,7 +121,7 @@
 		},
 
 		components : {
-            UserFollowing, Timeline, Activity, UserHome
+            UserFollowing, Timeline, Activity, UserHome, FileManager
         }
 	}
 </script>
