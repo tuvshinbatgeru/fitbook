16335cb61a9bec04e037cc3db2ac11badba73146
AkilLis <g.tuvshinbat@yahoo.com>
2016-11-18 16:48:57 +0800

    plan language support

--- resources/assets/js/lang/locales.js
+++ resources/assets/js/lang/locales.js
@@ -5,6 +5,43 @@ export default {
     manager : 'Менежер',
     teacher : 'Багш',
     reception : 'Ресепшен',
+    general: 'Үндсэн',
+    loyalty : 'Урамшуулал',
+    save : 'Хадгалах',
+    cancel : 'Цуцлах',
+    profile : {
+        following : 'дагасан',
+        followers : 'дагуулсан',
+        homeTab : 'Нүүр',
+        activityTab : 'Идэвх',
+        follow : 'Дагах', 
+        changeCover : 'Арын зураг солих'
+    },
+    activity : {
+        recently : 'ШИНЭЭР НЭМЭГДСЭН'
+    },
+    calendar : {
+      month : {
+          jan : '1-р сар',
+          feb : '2-р сар',
+          mar : '3-р сар',
+          apr : '4-р сар',
+          may : '5-р сар',
+          jun : '6-р сар',
+          jul : '7-р сар',
+          aug : '8-р сар',
+          sep : '9-р сар',
+          oct : '10-р сар',
+          nov : '11-р сар',
+          dec : '12-р сар',
+      },
+      day : {
+          mon : 'Дав',
+          wed : 'Лха',
+          fri : 'Баа',
+          sun : 'Ням'
+      }
+    }
   },
   en: {
     noresult : 'any result ...',
@@ -12,6 +49,43 @@ export default {
     manager : 'Manager',
     teacher : 'Teacher',
     reception : 'Reception',
+    general: 'General',
+    loyalty : 'Loyalty',
+    save : 'Save',
+    cancel : 'Cancel',
+    profile : {
+        following : 'following',
+        followers : 'followers',
+        homeTab : 'Home',
+        activityTab : 'Activity',
+        follow : 'Follow', 
+        changeCover : 'Change Cover'
+    },
+    activity : {
+        recently : 'RECENTLY ADDED PLANS'
+    },
+    calendar : {
+      month : {
+          jan : 'Jan',
+          feb : 'Feb',
+          mar : 'Mar',
+          apr : 'Apr',
+          may : 'May',
+          jun : 'Jun',
+          jul : 'Jul',
+          aug : 'Aug',
+          sep : 'Sep',
+          oct : 'Oct',
+          nov : 'Nov',
+          dec : 'Dec',
+      },
+      day : {
+          mon : 'Mon',
+          wed : 'Wed',
+          fri : 'Fri',
+          sun : 'Sun'
+      }
+    },
     message: {
       hello: 'the world',
       hoge: 'hoge',
