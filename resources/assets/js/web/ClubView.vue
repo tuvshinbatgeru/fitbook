83a465c11a1a2fb9f99a92b183e2352f11956386
AkilLis <g.tuvshinbat@yahoo.com>
2016-12-23 22:58:51 +0800

    menu, is-owner props added

--- resources/assets/js/web/ClubView.vue
+++ resources/assets/js/web/ClubView.vue
@@ -3,25 +3,59 @@
 	import HeaderDefault from '../actors/application/widgets/header/Default.vue';
 	import TeacherDefault from '../actors/application/widgets/content/TeacherDefault.vue';
 	import TrainingDefault from '../actors/application/widgets/content/TrainingDefault.vue';
+	import MemberDefault from '../actors/application/widgets/content/MemberDefault.vue';
 	import PlanDefault from '../actors/application/widgets/content/PlanDefault.vue';
 
 	export default {
 
 		props: { 
-			id : {},
+			id : {
+				required : true
+			},
+			menu : {
+				required : true
+			},
+			isManager : {
+				required : true
+			}
+		},
+
+		data () {
+			return {
+				content : {}
+			}
 		},
 
 		created : function () {
-			
+			this.setContent()
 		},
 
-		ready : function () {
-			
+		methods : {
+			menuChanged: function (menu) {
+				this.menu = menu
+				this.setContent()
+			},
+
+			setContent: function () {
+				switch(this.menu) {
+					case "home" : 
+						this.content = 'home-default'
+						break
+					case "plan" :
+						this.content = 'plan-default'
+						break	
+					case "member" :
+						this.content = 'member-default'
+						break
+					default : 
+						this.content = 'home-default'
+				} 
+			}
 		},
 
 		components : {
 			PlanDefault, HeaderDefault, 
-			TeacherDefault, TrainingDefault
+			TeacherDefault, TrainingDefault, MemberDefault
 		},
 	}
 </script>
