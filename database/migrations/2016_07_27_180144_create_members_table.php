5a2dd0698ae2237e36b2574310b104d09086171a
AkilLis <g.tuvshinbat@yahoo.com>
2016-12-23 22:56:39 +0800

    description fixed

--- database/migrations/2016_07_27_180144_create_members_table.php
+++ database/migrations/2016_07_27_180144_create_members_table.php
@@ -15,7 +15,7 @@ class CreateMembersTable extends Migration
         Schema::create('members', function (Blueprint $table) {
             $table->integer('club_id')->unsigned()->index();
             $table->integer('user_id')->unsigned()->index();
-            $table->tinyInteger('type'); //1. Club Manager 2.Club Teacher 3.Trainer 4.Ceo etc
+            $table->tinyInteger('type'); //1. Teacher 2.Manager 3.Trainer 4.Ceo etc
             $table->integer('view_order')->default(0);
             $table->timestamp('since_date');
             $table->timestamps();
