ac8144205c69a927799f1b59ac6676ce8b3d78e8
AkilLis <g.tuvshinbat@yahoo.com>
2016-12-23 23:02:20 +0800

    useless fields removed

--- resources/assets/js/actors/application/widgets/content/TrainingDefault.vue
+++ resources/assets/js/actors/application/widgets/content/TrainingDefault.vue
@@ -1,27 +1,5 @@
 <template>
-	<div class="row small-up-2 medium-up-3 large-up-4 small-centered">
-		<div class="column" v-for="item in trainings" style="background-color:#222222; color:#fff;">
-			<div>
-				<h4>
-					{{item.name}}
-				</h4>
-				{{item.description}}
-			</div>
-
-			<div class="row small-up-2 medium-up-3 large-up-4 small-centered">
-				<div class="column" v-for="teacher in item.teachers">
-					{{teacher.username}}
-					<img height="100" width="100" v-bind:src="teacher.avatar_url"/>
-				</div>
-			</div>
-
-			<div class="row small-up-2 medium-up-3 large-up-4 small-centered">
-				<div class="column" v-for="photo in item.photos">
-					<img height="400" width="400" v-bind:src="photo.url"/>
-				</div>
-			</div>			
-		</div>
-	</div>
+	
 </template>
 
 <script>
