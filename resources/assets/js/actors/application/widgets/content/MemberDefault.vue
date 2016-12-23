25ddd0a01e11901939a9507f9cee4899c231af95
AkilLis <g.tuvshinbat@yahoo.com>
2016-12-23 23:01:28 +0800

    request, active, archive members done

--- resources/assets/js/actors/application/widgets/content/MemberDefault.vue
+++ resources/assets/js/actors/application/widgets/content/MemberDefault.vue
@@ -4,54 +4,52 @@
 			<div class="fit-panel-title">
 				Members
 			</div>
-			<div class="fit-panel-filter">
-				<a class="filter">Reception</a>
-				<a class="filter">Member</a>
-				<a class="filter">Active</a>
+			<div class="fit-panel-filter" v-show="isManager == 'Y'">
+				<a @click="changeStatusType('request')" 
+					:class="statusType == 'request' ? 'active' : ''" 
+					class="filter">Request<span style="font-weight:bold">5</span> </a>
+				<a @click="changeStatusType('active')" 
+				   :class="statusType == 'active' ? 'active' : ''"
+				   class="filter">Active</a>
+				<a @click="changeStatusType('archive')" 
+				   :class="statusType == 'archive' ? 'active' : ''"
+				   class="filter">Archive</a>
 			</div>
 		</div>
 		<div class="fit-panel-body">
 			<div class="row manager">
 				<div class="columns large-6">
 					<div class="fit-panel-filter">
-						<a class="filter"><i class="fa fa-user"></i> Manager</a>
-						<a class="filter"><i class="fa fa-user-plus"></i>Reception</a>
-						<a class="filter"><i class="fa fa-male"></i> Teacher</a>
+						<a :class="type == 'manager' ? 'active' : ''"
+						   class="filter"
+						   @click="changeType('manager')">
+						   <i class="fa fa-user"></i> Manager
+						</a>
+						<a :class="type == 'reception' ? 'active' : ''"
+						   class="filter"
+						   @click="changeType('reception')">
+						   <i class="fa fa-user-plus"></i>Reception
+						</a>
+						<a :class="type == 'teacher' ? 'active' : ''"
+						   class="filter"
+						   @click="changeType('teacher')">
+						   <i class="fa fa-male"></i> Teacher
+						</a>
 					</div>
 				</div>
 				<div class="columns large-6" style="text-align: right;">
 					<label class="search-input">
-						<input type="text" placeholder="You can search people..."/>
-						<label>Search</label>
+						<input type="text" placeholder="You can search people..." v-model="searchText"/>
+						<label @click="searchMember()">Search</label>
 					</label>
 					<a class="fit-official-btn">+ Request</a>
 				</div>
 			</div>
 			<div class="row small-up-1 medium-up-2 large-up-3 large-collapse medium-collapse small-collapse">
 			  <div class="column column-block">
-			    <div class="member-card">
-					<a class="member-img">
-						<div class="cover-img">
-							<img class="horizontal" src="../images/prof.jpg" />
-							<span class="online"></span>
-						</div>
-					</a>
-					<div class="member-info">
-						<div class="_info">
-							<label class="_name">
-								О. Мандухай
-							</label>
-							<a class="_follow">135 followeres</a>
-							<i class="fa fa-circle"></i>
-							<a class="_follow">15 following</a>
-						</div>
-						<div class="follow-btn">
-							<a class="fit-hollow-btn">
-								+ Follow
-							</a>
-						</div>
-					</div>
-				</div>
+			  		<member-card v-for="member in members" :member="member">
+			  			
+			  		</member-card>
 			  </div>
 			</div>
 		</div>
@@ -59,25 +57,72 @@
 </template>
 
 <script>
+	import FilterAble from '../../mixins/Filterable'
+	import MemberCard from '../../components/MemberCard.vue'
+
 	export default {
+		mixins : [FilterAble],
+
 		props : {
 			id : { 
 				required: true,
 			},
-		},
 
-		created: function () {
-		    this.getTeachers();
-		},
-
-		data : function () {
-			return {
-				teachers : [],
+			isManager : {
+				required: true,
 			}
 		},
 
+		data() {
+			return {
+				members : [],
+				memberType : {},
+				searchText : '',
+				statusType : {},
+			}
+		},
+
+		created: function () {
+			this.setStatusType()
+			this.setType()
+			this.getMembers()
+		},
+
 		methods : {
-			
+			setType : function () {
+				this.type = this.getFilterValue('type', 'manager')
+			},
+
+			setStatusType : function () {
+				this.status_type = this.getFilterValue('status_type', 'request')
+			},
+
+			changeType : function (type) {
+				this.type = type
+				this.getMembers()
+			},
+
+			changeStatusType : function (type) {
+				this.status_type = type
+				this.getMembers()
+			},
+
+			searchMember : function () {
+
+			},
+
+			getMembers : function () {
+
+				this.$http.get(this.$env.get('APP_URI') + 'api/club/' + this.id + '/members?status_type=' + this.status_type + '&type=' + this.type).then(res => {
+				   	this.members = res.data.result
+				}).catch(err => {
+				  	
+				});
+			}
+		},
+
+		components : {
+			MemberCard
 		}
 	}
 </script>
