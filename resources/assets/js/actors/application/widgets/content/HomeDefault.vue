ef46570ee3a066457e25e6b5742a13a9a70128a7
AkilLis <g.tuvshinbat@yahoo.com>
2016-12-24 23:22:00 +0800

    home default component

---
+++ resources/assets/js/actors/application/widgets/content/HomeDefault.vue
@@ -0,0 +1,21 @@
+<template>
+	<h3>home default</h3>
+</template>
+<script>
+	import FilterAble from '../../mixins/Filterable'
+	
+	export default {
+		mixins : [FilterAble],
+
+		props : {
+			id : { 
+				required: true,
+			},
+
+			isManager : {
+				required: true,
+			}
+		},
+	}
+
+</script>
